1bf3fd8095a905395dba6b1270c5198621698286
FIlip 'Cherit' Szóstak <filip-szostak@wp.pl>
2021-11-25 09:36:03 +0100

    added launcher

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ def main():
     - action (generate, validate)
     """
     args = sys.argv
-    if len(args) >= 3:
+    if len(args) == 2:
+        use_action(args[1], Q4RSumWUAlgorithm())
+    elif len(args) >= 3:
         algorithm = select_algorithm(args[1])
         use_action(args[2], algorithm)
     else:
@@ -60,7 +62,7 @@ def use_action(action: str, algorithm: Algorithm):
             if file != '.gitkeep':
                 algorithm.generate_mock_result_file(file)
     else:
-        raise ValueError('Unknown action')
+        algorithm.schedule_tasks(action)
 
 
 if __name__ == '__main__':
